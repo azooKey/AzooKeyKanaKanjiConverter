76979ace5b794e77412000f0c9b412a07692a6d2
Buseong Kim <flight@skyline23.com>
2025-11-17 11:00:32 +0900

    Remove unusable TC

--- Tests/KanaKanjiConverterModuleTests/MainActorTestCase.swift
+++ Tests/KanaKanjiConverterModuleTests/MainActorTestCase.swift
@@ -2,14 +2,4 @@ import Dispatch
 import XCTest
 
 @MainActor
-class MainActorTestCase: XCTestCase {
-    override func invokeTest() {
-        if Thread.isMainThread {
-            super.invokeTest()
-        } else {
-            DispatchQueue.main.sync {
-                super.invokeTest()
-            }
-        }
-    }
-}
+class MainActorTestCase: XCTestCase {}
